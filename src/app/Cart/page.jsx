48953b8192ee466459ef0f39036730a9ed3819bb
Karthik Vishal <karthikvishal1506@gmail.com>
2025-08-12 11:19:27 +0530

    Denied page -reconstructed to sign in page alternative

--- src/app/Cart/page.jsx
+++ src/app/Cart/page.jsx
@@ -1,8 +1,29 @@
 "use client"
 
 import React from 'react'
+import { useSession } from "next-auth/react";
+import useUserInfo from "@/app/hooks/useUserInfo";
+import Link from "next/link";
 
 const page = () => {
+
+  const { data: session, status } = useSession();
+    const email = session?.user?.email;
+  
+    const { userInfo, loading, error } = useUserInfo(email);
+  
+
+  if (status === "loading" || loading) 
+    return(
+      <div className="relative items-center justify-center flex flex-col h-screen -top-20">
+        <p>Please Login to continue.</p>
+        <Link href="/signin">
+          <button className="text-blue-500">Sign In</button>
+          </Link>
+      </div>
+    );
+
+
   return (
     <div>
       Cart details..
--- src/app/Denied/page.jsx
+++ src/app/Denied/page.jsx
@@ -1,11 +1,17 @@
 "use client"
 
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Denied
+    <div className='flex items-center justify-center flex-col h-screen'>
+      <p>Please Log in to Continue</p>
+      <div className=''>
+      <Link href="/signin" className=''>
+        <button className='text-blue-500'>Sign in</button>
+      </Link>
+      </div>
     </div>
   )
 }
