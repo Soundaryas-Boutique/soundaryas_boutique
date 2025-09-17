4b7453a32586a25bfce7d19146450ae81d45b403
Krishnan S <kris2005f24@gmail.com>
2025-09-17 21:49:32 +0530

    converted MyReviewsPage from functional to class component for Review 2 requirement

--- src/app/(models)/SiteReview.js
+++ src/app/(models)/SiteReview.js
@@ -2,7 +2,7 @@ import { Schema } from "mongoose";
 
 const siteReviewSchema = new Schema({
   rating: { type: Number, required: true, min: 1, max: 5 },
-  comment: { type: String, trim: true }, // optional
+  comment: { type: String, trim: true }, 
   service: { type: String, trim: true },
   recommend: { type: String, trim: true },
   createdAt: { type: Date, default: Date.now },
