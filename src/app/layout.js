2f9cd2fb4b5391ca3602f6740d4b38f9f49d7875
Shibi404 <shibisbs123@gmail.com>
2025-10-12 09:30:06 +0530

    Boutique: Add Yeseva_One and Poppins font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,28 @@
 import "./globals.css";
 
-
 import AuthProvider from "../../components/AuthProvider";
 import { CartProvider } from "./context/CartContext";
-import { WishlistProvider } from "./context//WishlistContext";
+import { WishlistProvider } from "./context/WishlistContext";
 import NavbarWrapper from "../../components/NavbarWrapper";
 import IntroAnimation from "../../components/IntroAnimation";
+
+import { Poppins, Yeseva_One } from "next/font/google";
+
+// Poppins for body text
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["200","300","400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
+// Yeseva One for headings
+const yeseva = Yeseva_One({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-yeseva",
+});
 
 export const metadata = {
   title: "Soundarya's Boutique",
@@ -14,20 +31,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <AuthProvider>
-        <WishlistProvider>
-        <CartProvider>
-          <body className={` min-h-screen bg-white text-black font-main`}>
-            <IntroAnimation />
-            <NavbarWrapper />
-            <div className="min-h-screen">
-              {children}
-            </div>
-          </body>
-        </CartProvider>
-        </WishlistProvider>
-      </AuthProvider>
+    <html lang="en" className={`${poppins.variable} ${yeseva.variable}`}>
+      <body className="min-h-screen bg-white text-black font-main">
+        <AuthProvider>
+          <WishlistProvider>
+            <CartProvider>
+              <IntroAnimation />
+              <NavbarWrapper />
+              <div className="min-h-screen">{children}</div>
+            </CartProvider>
+          </WishlistProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
