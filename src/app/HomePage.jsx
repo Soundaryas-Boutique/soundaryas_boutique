15b5535be8c0919469d18d287d9f69f9ea53b1b0
Shibi404 <shibisbs123@gmail.com>
2025-10-12 23:37:47 +0530

    Boutique: Add `export const dynamic = "force-dynamic"` to ensure homepage always displays latest products.

--- src/app/HomePage.jsx
+++ src/app/HomePage.jsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import SlidingBanner from "../../components/SlidingBanner";
 import SareeCard from "../../components/SareeCard";
